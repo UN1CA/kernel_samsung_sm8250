58058b71d7c3650a3fa2a11e9d807375bc46854d
Atakan <ata.kaner03@gmail.com>
2026-09-23 21:27:21 +0300

    techpack: display: Checkout to G780GXXSFEYC1
    * Keep compatibility with the changes of upstream kernel.
    
    Change-Id: I43a9ee9912dd4b78b958edd5ad2126d996362bf1

--- techpack/display/msm/sde/sde_encoder_phys_cmd.c
+++ techpack/display/msm/sde/sde_encoder_phys_cmd.c
@@ -644,14 +644,12 @@ static int _sde_encoder_phys_cmd_handle_ppdone_timeout(
 				pending_kickoff_cnt);
 
 		SDE_EVT32(DRMID(phys_enc->parent), SDE_EVTLOG_FATAL);
-		mutex_lock(phys_enc->vblank_ctl_lock);
 		sde_encoder_helper_unregister_irq(phys_enc, INTR_IDX_RDPTR);
 		if (sde_kms_is_secure_session_inprogress(phys_enc->sde_kms))
 			SDE_DBG_DUMP("secure", "all", "dbg_bus");
 		else
 			SDE_DBG_DUMP("all", "dbg_bus", "vbif_dbg_bus");
 		sde_encoder_helper_register_irq(phys_enc, INTR_IDX_RDPTR);
-		mutex_unlock(phys_enc->vblank_ctl_lock);
 	}
 
 	/*
@@ -1077,7 +1075,7 @@ static int _get_tearcheck_threshold(struct sde_encoder_phys *phys_enc,
 
 		if (phys_enc->parent_ops.get_qsync_fps)
 			phys_enc->parent_ops.get_qsync_fps(
-				phys_enc->parent, &qsync_min_fps, 0);
+				phys_enc->parent, &qsync_min_fps);
 
 		if (!qsync_min_fps || !default_fps || !yres) {
 			SDE_ERROR_CMDENC(cmd_enc,
